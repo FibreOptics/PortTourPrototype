b976c6688436e09e623e1eeb3ad50bb435656385
FibreOptics <nacalon@hotmail.com>
2019-11-13 17:35:58 +0700

    Updates

---
+++ precache-manifest.dfab378ca7ed09d47ac33fecfd4cc911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11faedb7af3330e067f2e9a9ea0de37",
+    "url": "/PortTourPrototype/index.html"
+  },
+  {
+    "revision": "c8579b42be761a8083d8",
+    "url": "/PortTourPrototype/static/css/main.7caef05f.chunk.css"
+  },
+  {
+    "revision": "fb11d089bd07bd0df990",
+    "url": "/PortTourPrototype/static/js/2.07ef78bd.chunk.js"
+  },
+  {
+    "revision": "c8579b42be761a8083d8",
+    "url": "/PortTourPrototype/static/js/main.95e4a8d7.chunk.js"
+  },
+  {
+    "revision": "081aa072d96433305a4f",
+    "url": "/PortTourPrototype/static/js/runtime-main.78d4b117.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.679ea3cb40cbc1be0934f7b864a4e620.js"
+  "/PortTourPrototype/precache-manifest.dfab378ca7ed09d47ac33fecfd4cc911.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PortTourPrototype/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
